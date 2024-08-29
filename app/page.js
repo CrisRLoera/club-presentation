c0dc28f4c6130b176a6b347a429c5c8bd08445d3
CrisRLoera <rcristianrubi@gmail.com>
2024-08-28 22:59:33 -0600

    second and third curse

--- app/page.js
+++ app/page.js
@@ -8,6 +8,12 @@ export default function Home() {
       <div className="flex justify-center py-5">
         <img src='/5.png' style={{ width: '500px', height: '500px', borderRadius:'2%' }} alt='Git Curse' />
       </div>
+      <div className="flex justify-center py-5">
+        <img src='/6.png' style={{ width: '500px', height: '500px', borderRadius:'2%' }} alt='Git Curse' />
+      </div>
+      <div className="flex justify-center py-5">
+        <img src='/7.png' style={{ width: '500px', height: '500px', borderRadius:'2%' }} alt='Git Curse' />
+      </div>
     </div>
   );
 }
