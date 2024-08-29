ec5eb2c754a7e1e73ec44cb2135704ec57dfa07a
CrisRLoera <rcristianrubi@gmail.com>
2024-08-28 23:09:49 -0600

    final adjustments

--- app/page.js
+++ app/page.js
@@ -6,14 +6,15 @@ export default function Home() {
       <h1 className="flex justify-center text-5xl font-mono py-5">Club de Programación</h1>
       <h2 className="flex justify-center text-xl font-mono py-5">¡Bienvenido a la página del club de programación!</h2>
       <div className="flex justify-center py-5">
-        <img src='/5.png' style={{ width: '500px', height: '500px', borderRadius:'2%' }} alt='Git Curse' />
+        <img src='/5.png' style={{ width: '500px', height: '500px', borderRadius:'2%' }} alt='Git Course' />
       </div>
       <div className="flex justify-center py-5">
-        <img src='/6.png' style={{ width: '500px', height: '500px', borderRadius:'2%' }} alt='Git Curse' />
+        <img src='/6.png' style={{ width: '500px', height: '500px', borderRadius:'2%' }} alt='Angular Course' />
       </div>
       <div className="flex justify-center py-5">
-        <img src='/7.png' style={{ width: '500px', height: '500px', borderRadius:'2%' }} alt='Git Curse' />
+        <img src='/7.png' style={{ width: '500px', height: '500px', borderRadius:'2%' }} alt='Linux Course' />
       </div>
+      <p className="flex justify-center text-xl font-mono py-5 text-lime-500">&lt; Este es un espacio para ti &gt;</p>
     </div>
   );
 }
